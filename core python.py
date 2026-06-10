1a02adbafdc8a166a2c414f46bfce14c26c3f705
Annapureddy Chaithanya Kumar Reddy <annapureddychaithanya@gmail.com>
2026-06-10 14:10:52 +0530

    core python.py

--- core python.py
+++ core python.py
@@ -1,12 +1,12 @@
-# from functools import reduce
-# a=[1,2,3,4,5,6,7,8,9,10]
-# k=list(map(lambda x:x**2,a))
-# m=list(filter(lambda x:x%2==1,k))
-# n=reduce(lambda x,y:x+y,m,0)
-# print(n)
-#
-# l=reduce(lambda x,y:x+y,filter(lambda x:x%2==1,map(lambda x:x**2,a)))
-# print(l)
+from functools import reduce
+a=[1,2,3,4,5,6,7,8,9,10]
+k=list(map(lambda x:x**2,a))
+m=list(filter(lambda x:x%2==1,k))
+n=reduce(lambda x,y:x+y,m,0)
+print(n)
+
+l=reduce(lambda x,y:x+y,filter(lambda x:x%2==1,map(lambda x:x**2,a)))
+print(l)
 
 
 class student:
